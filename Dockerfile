9362b89fe0d1af171ec48f46bba9e08735dbc4b6
Senthil Kumar Palanivelu <senthilkumar_palanivelu@trimble.com>
2021-07-29 21:31:59 +0530

    Docker Updates   - MultiStage Docker Build

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,42 @@
-FROM node:16
+#
+# ---- Base Node ----
+FROM alpine:latest AS base
+# install node
+RUN apk add --no-cache nodejs-current npm tini
+# set working directory
+WORKDIR /root/app
+# Set tini as entrypoint
+ENTRYPOINT ["/sbin/tini", "--"]
+# copy project file
+COPY package.json .
+ 
+#
+# ---- Dependencies ----
+FROM base AS dependencies
+# install node packages
+RUN npm set progress=false && npm config set depth 0
+RUN npm install --only=production 
+# copy production node_modules aside
+RUN cp -R node_modules prod_node_modules
+# install ALL node_modules, including 'devDependencies'
+RUN npm install
+ 
+##
+## ---- Test ----
+## run linters, setup and tests
+#FROM dependencies AS test
+#COPY . .
+#RUN  npm run lint && npm run setup && npm run test
+ 
+#
+# ---- Release ----
+FROM base AS release
+# copy production node_modules
 WORKDIR /usr/src/app
 
-
-COPY package*.json ./
-
-RUN npm install
-
+COPY --from=dependencies /root/app/prod_node_modules ./node_modules
+# copy app sources
 COPY . .
-
-EXPOSE 50051
-CMD [ "node", "source/server.js" ]
-
-
+# expose port and define CMD
+EXPOSE 5000
+CMD npm run start
--- source/server.js
+++ source/server.js
@@ -1,5 +1,5 @@
 const logger      = require('./utils/LoadData')
-const grpc        = require('grpc');
+const grpc        = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 
 const packageDefElement = protoLoader.loadSync("./protos/element.proto", {});
@@ -7,32 +7,33 @@ const elementProto      = grpc.loadPackageDefinition(packageDefElement);
 const elementPackage    = elementProto.element;
 
 const path        = require('path');
-const dataDirPath = path.join(__dirname, './data/');
+const dataDirPath = path.join(path.resolve("./"), './data/');
 const dataStores  = logger.loadFolder(dataDirPath);
+var port = process.env.PORT || 5000;
 
 const server = new grpc.Server();
-server.bind('0.0.0.0:8088', grpc.ServerCredentials.createInsecure());
+server.bindAsync(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure(),error => {
+    server.addService(elementPackage.DataService.service, {
+        getData: (_, callback) => {
+            const DataSource = _.request.hasOwnProperty("source") ? _.request.source : "default";
+            const size       = _.request.hasOwnProperty("size") ? _.request.size : 1;
+            if (dataStores.hasOwnProperty(DataSource)) {
+                callback(null, {chunks: dataStores[DataSource].get(size)});
+            } else {
+                callback(null, {chunks: []});
+            }
+        },
+        addData: (_, callback) => {
+            const DataSource = _.request.hasOwnProperty("source") ? _.request.source : "default";
+            const chunks     = _.request.hasOwnProperty("chunks") ? _.request.chunks : ["data"];
+            if (dataStores.hasOwnProperty(DataSource) && Array.isArray(chunks)) {
+                callback(null, {chunks: dataStores[DataSource].put(chunks)});
+            } else {
+                callback(null, {chunks: []});
+            }
+        }
+    });
 
-server.addService(elementPackage.DataService.service, {
-    getData: (_, callback) => {
-        const DataSource = _.request.hasOwnProperty("source") ? _.request.source : "default";
-        const size       = _.request.hasOwnProperty("size") ? _.request.size : 1;
-        if (dataStores.hasOwnProperty(DataSource)) {
-            callback(null, {chunks: dataStores[DataSource].get(size)});
-        } else {
-            callback(null, {chunks: []});
-        }
-    },
-    addData: (_, callback) => {
-        const DataSource = _.request.hasOwnProperty("source") ? _.request.source : "default";
-        const chunks     = _.request.hasOwnProperty("chunks") ? _.request.chunks : ["data"];
-        if (dataStores.hasOwnProperty(DataSource) && Array.isArray(chunks)) {
-            callback(null, {chunks: dataStores[DataSource].put(chunks)});
-        } else {
-            callback(null, {chunks: []});
-        }
-    }
+    server.start()
+    console.log(`Server running at 0.0.0.0:${port}`);
 });
-
-server.start()
-console.log('Server running at http://0.0.0.0:8088');
